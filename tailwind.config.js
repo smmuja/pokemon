6d87347e1fcf99c39b5208439ea8d57eb9283e3c
smmuja <smmuja27@gmail.com>
2024-08-04 20:32:56 +0700

    style: config custom color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,37 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+      pokemon: ["pokemon"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      ghostWhite: "f8f8ff",
+      black: "010101",
+      midnight: "#121063",
+
+      reddishOrange: "#ff0000",
+      red: "#cc0000",
+      blue: "#3b4cca",
+      yellow: "	#ffde00",
+      tanYellow: "#b3a125",
+
+      lightBlue: "#30A7D7",
+      pink: "FF90C4",
+      green: "4DAD5B",
+      purple: "B38AEA",
+
+      gray: "4B4A49",
+      lighGray: "616161",
+    },
+
     extend: {},
   },
   plugins: [],
